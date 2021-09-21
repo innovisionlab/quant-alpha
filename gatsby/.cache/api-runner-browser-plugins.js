6b45f4982927c0313955d4cc7558d27969ac3c2a
innovisionlab <90440732+innovisionlab@users.noreply.github.com>
2021-09-21 16:31:43 +0500

    Delete gatsby/.cache directory

--- gatsby/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-source-wordpress/gatsby-browser.js'),
-      options: {"plugins":[],"url":"https://quantalphanet.wpcomstaging.com/graphql","verbose":true},
-    }]
